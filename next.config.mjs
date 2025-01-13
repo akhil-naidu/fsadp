9ee8012e11f7256c52d4b9f5d933ea4c55fa702c
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2025-01-13 08:30:54 +0530

    chore: added config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  assetPrefix:
-    'https://custom-webpage-static-assets-production.fermion.app/677929d6d8b9c805a50be566-custom-zip-pages/',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.vercel-storage.com',
-        port: '',
-        search: '',
-      },
-    ],
-  },
+  // output: 'export',
+  // assetPrefix:
+  //   'https://custom-webpage-static-assets-production.fermion.app/677929d6d8b9c805a50be566-custom-zip-pages/',
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
