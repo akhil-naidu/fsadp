590e30145b74591148344b83b12a5212b726c63d
girish chalumuri <girishchalumuri@gmail.com>
2025-01-27 23:17:42 +0530

    hydration error fixes

--- app/curriculum/page.tsx
+++ app/curriculum/page.tsx
@@ -12,11 +12,17 @@ const page = () => {
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
             Foundational Concepts
           </h3>
-          <li className="pt-2">Introduction to HTML & CSS</li>
-          <li className="pt-2">What is HTML? What is CSS?</li>
-          <li className="pt-2">
-            Basic HTML structure CSS Basics (Inline, Internal, External CSS)
-          </li>
+          <ul>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Introduction to HTML & CSS
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              What is HTML? What is CSS?
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Basic HTML structure CSS Basics (Inline, Internal, External CSS)
+            </li>
+          </ul>
         </li>
         <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -26,18 +32,26 @@ const page = () => {
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
             HTML & CSS Fundamentals
           </h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            <li className="pt-2">
+          <ul>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
               Text Formatting, Lists, Links, Images, Tables
             </li>
-            <li className="pt-2">Forms & Input Elements</li>
-            <li className="pt-2">Selectors & Specificity</li>
-            <li className="pt-2">
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Forms & Input Elements
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Selectors & Specificity
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
               Box Model (Margin, Padding, Border, Content)
             </li>
-            <li className="pt-2">Colors & Backgrounds</li>
-            <li className="pt-2">Typography (Fonts, Text Styling)</li>
-          </p>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Colors & Backgrounds
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Typography (Fonts, Text Styling)
+            </li>
+          </ul>
         </li>
         <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -47,14 +61,29 @@ const page = () => {
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
             Intermediate HTML & CSS
           </h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            <li className="pt-2">Semantic HTML</li>
-            <li className="pt-2">Media & Embedding</li>
-            <li className="pt-2">Attaching HTML and CSS in different ways</li>
-            <li className="pt-2">Flexbox & Grid Layout</li>
-            <li className="pt-2">Positioning & Display Properties</li>
-            <li className="pt-2">Transitions & Animations</li>
-          </p>
+          <ul>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Semantic HTML
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Semantic HTML
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Media & Embedding
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Attaching HTML and CSS in different ways
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Flexbox & Grid Layout
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Positioning & Display Properties
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Transitions & Animations
+            </li>
+          </ul>
         </li>
         <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -64,18 +93,26 @@ const page = () => {
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
             Advanced Concepts
           </h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            <li className="pt-2">Performance Optimization</li>
-            <li className="pt-2">Forms Validation</li>
-            <li className="pt-2">Web Components</li>
-            <li className="pt-2">
+          <ul>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Performance Optimization
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Forms Validation
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Web Components
+            </li>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
               Responsive Design (Media Queries, Mobile-First Approach)
             </li>
-            <li className="pt-2">
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
               CSS Variables & Preprocessors (SASS Basics)
             </li>
-            <li className="pt-2">Attaching Javascript and HTML </li>
-          </p>
+            <li className="pt-2 text-base font-normal text-gray-500 dark:text-gray-400">
+              Attaching Javascript and HTML{' '}
+            </li>
+          </ul>
         </li>
       </ol>
     </div>
